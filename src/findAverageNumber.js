af361fe510b03293309e6eab5d84e8d7710e99f2
cynthiacodes <cjnkstudy01@hotmail.com>
2023-06-22 10:17:36 +0100

    initial commit

---
+++ src/findAverageNumber.js
@@ -0,0 +1,26 @@
+/* 
+Function findAverageNumber
+
+Input: numbersArray containing negative and positive numbers
+
+declare
+- sumOfPositiveNum = 0
+- positiveNumCount = 0
+- sumOfNegativeNum = 0
+- negativeNumCount = 0
+
+For let num of Number
+    if num > 0
+       sumOfPositiveNum += num
+        positiveNumCount += 1
+    else
+        sumOfNegativeNum += 0
+        NegativeNumCount += 0
+
+avgPositiveNum = sumOfPositiveNum/positiveNumCount
+
+avgNegativeNum = sumOfNegativeNum/NegativeNumCount
+
+return averageNumArray = [avgpositive, avgnegative]
+
+*/
